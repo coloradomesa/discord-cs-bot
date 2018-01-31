0b93e842d663a56c116aff608e53daa9443218b7
jtcressy <joel@jtcressy.net>
2018-01-30 19:36:50 -0700

    implement \clean command

--- cogs/chatjanitor.py
+++ cogs/chatjanitor.py
@@ -10,10 +10,11 @@ class ChatJanitor:
     def __init__(self, bot: discord.ext.commands.Bot):
         self.bot = bot
 
-    @commands.command(hidden=True)
+    @commands.command(hidden=True, pass_context=True)
     async def clean(self, ctx, count):
-        ""
-
+        """Clear a chat channel of X lines"""
+        count = int(count)
+        await self.bot.purge_from(ctx.message.channel, limit=count)
 
 def setup(bot):
     bot.add_cog(ChatJanitor(bot))
